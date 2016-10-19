8a1d36357716b31e541c2b9b7b9fcea5f28503d3
Johnson Ho <m97_j@DESKTOP-QUMH9HN>
2016-10-19 23:45:38 +0200

    thread safety: init

---
+++ Realtid/_01Threading/Comps/Foo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Threading.Comps
+{
+    public class Foo
+    {
+        private static object locker = new object();
+
+        int[] numbers;
+
+        public Foo(int size = 50)
+        {
+            numbers = new int[size];
+        }
+
+        public void FillInt(int num)
+        {
+            lock (locker)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = num;
+                }
+                Print();
+            }
+        }
+
+        public void Fill(object mObj)
+        {
+            if (mObj is int)
+                FillInt((int)mObj);
+
+
+            //lock (locker)
+            //{
+            //    if (mObj is int)
+            //    {
+            //        for (int i = 0; i < numbers.Length; i++)
+            //        {
+            //            numbers[i] = (int)mObj;
+            //        }
+            //        Print();
+            //    }
+            //}
+
+        }
+
+        public void Print()
+        {
+            foreach (int i in numbers)
+            {
+                Console.Write(numbers[i]);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Realtid/_01Threading/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using _01Threading.Comps;
+
+namespace _01Threading
+{
+    class Program
+    {
+        static readonly object locker = new object();
+
+        static int[] numbers = new int[50];
+
+        static void Main(string[] args)
+        {
+            FirstCase();
+            Console.ReadKey();
+            SecondCase();
+        }
+
+        static void SecondCase()
+        {
+            Console.WriteLine("Second case:");
+
+            var foo = new Foo();
+            new Thread(() => foo.Fill(3)).Start();
+            var thread = new Thread(foo.Fill);
+            thread.Start(5);
+            foo.FillInt(7);
+        }
+
+        static void FirstCase()
+        {
+            Console.WriteLine("First case:");
+            new Thread(FillA).Start();
+            for (int i = 0; i < 20; i++)
+            {
+                FillB(9);
+            }
+        }
+
+        static void FillA()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                FillB(0);
+            }
+        }
+
+
+        static void FillB(int n)
+        {
+            lock (locker)
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    numbers[i] = n;
+                }
+                foreach (int i in numbers)
+                {
+                    Console.Write(numbers[i]);
+                }
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
