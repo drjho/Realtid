b031ee4b49aad19e0c674023ed9c0563735b2fa3
Johnson Ho <m97_j@DESKTOP-QUMH9HN>
2016-10-30 00:19:30 +0200

    first commit: seems faulty

---
+++ Euler/Euler/Problems/MultiplesOf3And5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Problems
+{
+    class MultiplesOf3And5
+    {
+        public static int GetResult(int limit)
+        {
+            int sum = 0;
+            int current = 3;
+            while (current < limit)
+            {
+                sum += current;
+                current += 3;
+            }
+            current = 5;
+            while (current < limit)
+            {
+                sum += current;
+                current += 5;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Euler/Euler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Euler.Problems;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MultiplesOf3And5.GetResult(1000));
+        }
+    }
+}
